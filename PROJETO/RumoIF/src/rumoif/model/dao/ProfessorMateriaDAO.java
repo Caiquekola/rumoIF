c6452fef99400544daec22b90a65bb32f5a2ac62
Caio Rievers Duarte <131414483+cRievers@users.noreply.github.com>
2024-01-03 17:32:51 -0300

    Add files via upload

--- PROJETO/RumoIF/src/rumoif/model/dao/ProfessorMateriaDAO.java
+++ PROJETO/RumoIF/src/rumoif/model/dao/ProfessorMateriaDAO.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rumoif.model.dao;
 
 /**
  *
  * @author Caio Rievers
  */
+
+import rumoif.connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import rumoif.model.bean.ProfessorMateria;
+
 public class ProfessorMateriaDAO {
     
+    public void create(ProfessorMateria profMateria){
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        String sql = ("INSERT INTO rumoif.professor_materia (id_professor,id_materia) VALUES (?,?)");
+        
+        try {
+            
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, profMateria.getId_professor());
+            stmt.setInt(2, profMateria.getId_materia());
+            
+            stmt.executeUpdate();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ProfessorMateriaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+        
+    }
 }
