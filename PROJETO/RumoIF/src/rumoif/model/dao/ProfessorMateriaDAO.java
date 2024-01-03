a2db33ce4d3723baaeef1a132584c39486d42ea6
Caio Rievers Duarte <131414483+cRievers@users.noreply.github.com>
2024-01-03 17:28:15 -0300

    Add files via upload

---
+++ PROJETO/RumoIF/src/rumoif/model/dao/ProfessorMateriaDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rumoif.model.dao;
+
+/**
+ *
+ * @author Caio Rievers
+ */
+public class ProfessorMateriaDAO {
+    
+}
