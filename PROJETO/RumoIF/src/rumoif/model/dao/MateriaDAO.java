6ba9281efaf035dfbc0c994387f2f8560777113b
Caio Rievers Duarte <131414483+cRievers@users.noreply.github.com>
2024-01-03 15:07:03 -0300

    Add files via upload

--- PROJETO/RumoIF/src/rumoif/model/dao/MateriaDAO.java
+++ PROJETO/RumoIF/src/rumoif/model/dao/MateriaDAO.java
@@ -6,9 +6,12 @@ package rumoif.model.dao;
 
 import rumoif.connection.ConnectionFactory;
 import rumoif.model.bean.Materia;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,5 +31,63 @@ public class MateriaDAO {
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
+    }
+    
+    public List<Materia> read() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = ("SELECT * FROM rumoif.materia");
+        List<Materia> materias = new ArrayList<Materia>();
+        try {
+            stmt = con.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Materia m = new Materia(rs.getString("nome_materia"), rs.getInt("id_materia"));
+                materias.add(m);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return materias;
+    }
+    
+    public void update(Materia m) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        String sql = ("UPDATE rumoif.materia SET nome_materia = ? WHERE id_materia = ?");
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, m.getNome_materia());
+            stmt.setInt(2, m.getId_materia());
+            
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+
+    }
+    
+    public void delete(Materia m) {
+
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            String sql = ("DELETE FROM rumoif.materia WHERE nome_materia = ? AND id_materia = ?");
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, m.getNome_materia());
+            stmt.setInt(2, m.getId_materia());
+            stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+
     }
 }
